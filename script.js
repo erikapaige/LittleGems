53bd694c3c0d10f9833a718cb685062baa85dc17
Nathan Mc <nathanmcneil@pm.me>
2020-05-04 10:44:13 -0700

    created a js file and made a fetch for geolocation api.

---
+++ script.js
@@ -0,0 +1,14 @@
+let lat = 0
+let lon = 0
+
+navigator.geolocation.getCurrentPosition(position => {
+ let lat = position.coords.latitude
+ let lon = position.coords.longitude
+ console.log(lat)
+ console.log(lon)
+})
+
+fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=2260330a175994e3a7136d694876386f`)
+ .then(displayWeather => {
+
+ })
