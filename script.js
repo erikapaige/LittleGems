ffd5d159a549c7ae8de2db8085907a2d12ed96c1
Nathan Mc <nathanmcneil@pm.me>
2020-05-04 10:52:35 -0700

    commenteed out an fetch request template

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ navigator.geolocation.getCurrentPosition(position => {
  console.log(lon)
 })
 
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=2260330a175994e3a7136d694876386f`)
- .then(displayWeather => {
+// fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=2260330a175994e3a7136d694876386f`)
+//  .then(displayWeather => {
 
- })
+//  })
