ddeba8e0f7bb94c9152752512d5fd7cc0954d213
Erika <erika.paige.handley@gmail.com>
2020-05-05 14:02:59 -0700

    trying userPosition

--- script.js
+++ script.js
@@ -16,30 +16,30 @@ function initMap() {
   console.log(userPosition.lat)
   console.log(userPosition.lng)
 
-  function success(position) {
-   // The map, centered at user position
-   let map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 13, center: userPosition })
-   // The marker, positioned at user position
-   let marker = new google.maps.Marker({ position: userPosition, map: map })
-   console.log(irvine)
-  }
+  // function success(position) {
+  //  // The map, centered at user position
+  //  let map = new google.maps.Map(
+  //   document.getElementById('map'), { zoom: 13, center: userPosition })
+  //  // The marker, positioned at user position
+  //  let marker = new google.maps.Marker({ position: userPosition, map: map })
+  //  console.log(irvine)
+  // }
   
-  function error() {
-   // The map, centered at user position
-   let map = new google.maps.Map(
-    document.getElementById('map'), { zoom: 13, center: irvine })
-   // The marker, positioned at user position
-   let marker = new google.maps.Marker({ position: irvine, map: map })
-   console.log(irvine)
-  }
+  // function error() {
+  //  // The map, centered at user position
+  //  let map = new google.maps.Map(
+  //   document.getElementById('map'), { zoom: 13, center: irvine })
+  //  // The marker, positioned at user position
+  //  let marker = new google.maps.Marker({ position: irvine, map: map })
+  //  console.log(irvine)
+  // }
 
-  const options = {
-   enableHighAccuracy: true,
-   maximumAge: 30000,
-   timeout: 27000
-  }
+  // const options = {
+  //  enableHighAccuracy: true,
+  //  maximumAge: 30000,
+  //  timeout: 27000
+  // }
   
-  const watchID = navigator.geolocation.watchPosition(success, error, options)
+  // const watchID = navigator.geolocation.watchPosition(success, error, options)
  })
 }
