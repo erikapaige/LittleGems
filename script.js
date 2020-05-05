7c87e5d5630fbcab80e020fd86c0581d71cefc06
Erika <erika.paige.handley@gmail.com>
2020-05-05 14:07:32 -0700

    testing success

--- script.js
+++ script.js
@@ -10,21 +10,19 @@ let irvine = {
 // Initialize and add the map
 function initMap() {
  //get users location and assigns it to empty object created earlier.
- navigator.geolocation.getCurrentPosition(position => {
+  navigator.geolocation.getCurrentPosition(success, position => {
   userPosition.lat = position.coords.latitude
   userPosition.lng = position.coords.longitude
   console.log(userPosition.lat)
   console.log(userPosition.lng)
 
   //  success, error, options,
-  // function success(position) {
-  //  // The map, centered at user position
-  //  let map = new google.maps.Map(
-  //   document.getElementById('map'), { zoom: 13, center: userPosition })
-  //  // The marker, positioned at user position
-  //  let marker = new google.maps.Marker({ position: userPosition, map: map })
-  //  console.log(irvine)
-  // }
+  function success(position) {
+   // The map, centered at user position
+   let map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 13, center: userPosition })
+   // The marker, positioned at user position
+   let marker = new google.maps.Marker({ position: userPosition, map: map })}
   
   // function error() {
   //  // The map, centered at user position
