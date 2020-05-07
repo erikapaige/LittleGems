094a67a2f19b6eb38d53b34e557f3986a84842cc
Erika <erika.paige.handley@gmail.com>
2020-05-07 13:34:01 -0700

    updated instagram photo and BG image

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function initMap() {
           }
 
           // Checks if restaurant has photo or not, if it does sets img source to that, if it doesn't sets img source to placeholder
-          if (data.restaurants[i].restaurant.photo_count === 0 || data.restaurants[i].restaurant.thumb === '' || data.restaurants[i].restaurant.thumb === "403") {
+          if (data.restaurants[i].restaurant.photo_count === 0 || data.restaurants[i].restaurant.thumb === '' || data.restaurants[i].restaurant.photos[0].photo.url.includes('instagram')) {
             document.getElementById(`img${i}`).src = "Assets/images/placeholder_1000px.png";
           } else {
             document.getElementById(`img${i}`).src = data.restaurants[i].restaurant.photos[0].photo.url;
@@ -160,3 +160,94 @@ document.getElementById('searchInput').addEventListener('keypress', function () 
 }
 
 // END OF GEOLOCATION
+
+//empty array to later store saved restaurants in local storage
+let favoriteArray = []
+
+//collect restaurant's unique ID, stores it in array, and then saves array to local storage
+function saveRestaurant() {
+  let restaurantID = event.target.id
+  favoriteArray.push(restaurantID)
+  localStorage.setItem('favoriteArray', JSON.stringify(favoriteArray))
+}
+// document.addEventListener('click', (event) => {
+//   if (event.target.className === 'savedGem') {
+//     function retrieveSaved() {
+//       localStorage.getItem('favoriteArray', JSON.parse(favoriteArray))
+//       for (i = 0; i < favoriteArray.length; i++) {
+//         restaurantID[i] = favoriteArray[i]
+//         console.log(restaurantID[i])
+//         $.ajax({
+//           type: "GET", //it's a GET request API
+//           headers: {
+//             'X-Zomato-API-Key': '6cc636d36121906ab8ce98c1468d462a' //only allowed non-standard header
+//           },
+//           url: `https://developers.zomato.com/api/v2.1/restaurant?res_id=${restaurantID[i]}`, //what do you want
+//           dataType: 'json', //wanted response data type - let jQuery handle the rest...
+//           data: {
+//             //could be directly in URL, but this is more pretty, clear and easier to edit 
+//           },
+//           processData: true, //data is an object => tells jQuery to construct URL params from it
+//           success: function (data) {
+//             console.log(data);
+//             // Continues to loop through Zomato API and create elements until we have 8 cards on our page
+//             for (i = 1; document.getElementById('row1').childElementCount + document.getElementById('row2').childElementCount < 8; i++) {
+//               // Checks if restaurants rating is above 3 stars with less than 40 total ratings
+//               if (data.restaurants[i].restaurant.user_rating.aggregate_rating > 3 && data.restaurants[i].restaurant.user_rating.votes < 40) {
+//                 // Checks that the div row1 has less than 4 cards, if it has 4 the next 4 cards are added to row 2 with the else statement
+//                 if (document.getElementById('row1').childElementCount < 4) {
+//                   let row = (document.getElementById('row1'));
+//                   let gemElem = document.createElement('DIV');
+//                   row.appendChild(gemElem);
+//                   gemElem.setAttribute("id", `card${ i }`);
+//                   gemElem.classList.add("col", "s12", "m3");
+//                   gemElem.innerHTML = `
+//             <div class="card z-depth-2" id="restauraunt${i}">
+//               <div class="card-image">
+//                 <img id="img${i}" src="" alt="restaurant option ${i}">
+//               </div>
+//               <div class="card-content2">
+//                 <span class="card-title" id="title${i}">${data.restaurants[i].restaurant.name}</span>
+//                 <p id="cuisine${i}">${data.restaurants[i].restaurant.cuisines}</p>
+//                 <p id="rating${i}">${data.restaurants[i].restaurant.user_rating.aggregate_rating} (${data.restaurants[i].restaurant.user_rating.votes})</p>
+//                 <p id="address${i}">${data.restaurants[i].restaurant.location.address}</p>
+//                 <a class="waves-effect waves-light btn" href="${data.restaurants[i].restaurant.url}" id="link${i}" target="_blank">Go To Restaurant</a>
+//               </div>
+//             </div>
+//                   `
+//                 } else {
+//                   row = (document.getElementById('row2'));
+//                   gemElem = document.createElement('DIV');
+//                   row.appendChild(gemElem);
+//                   gemElem.setAttribute("id", `card${ i }`);
+//                   gemElem.classList.add("col", "s12", "m3");
+//                   gemElem.innerHTML = `
+//               <div class="card z-depth-2" id="restauraunt${i}">
+//                 <div class="card-image">
+//                   <img id="img${i}" src="" alt="restaurant option ${i}">
+//                 </div>
+//                 <div class="card-content2">
+//                   <span class="card-title" id="title${i}">${data.restaurants[i].restaurant.name}</span>
+//                   <p id="cuisine${i}">${data.restaurants[i].restaurant.cuisines}</p>
+//                   <p id="rating${i}">${data.restaurants[i].restaurant.user_rating.aggregate_rating} (${data.restaurants[i].restaurant.user_rating.votes})</p>
+//                   <p id="address${i}">${data.restaurants[i].restaurant.location.address}</p>
+//                   <a class="waves-effect waves-light btn" href="${data.restaurants[i].restaurant.url}" id="link${i}" target="_blank">Go To Restaurant</a>
+//                 </div>
+//               </div>`
+//                 }
+//                 // Checks if restaurant has photo or not, if it does sets img source to that, if it doesn't sets img source to placeholder
+//                 if (data.restaurants[i].restaurant.photo_count === 0 || data.restaurants[i].restaurant.thumb === '') {
+//                   document.getElementById(`img${ i }`).src = "Assets/placeholder_Green_1000px.png";
+//                 } else {
+//                   document.getElementById(`img${ i }`).src = data.restaurants[i].restaurant.photos[0].photo.thumb_url;
+//                 }
+//               }
+//             }
+//           }
+//       })
+//     }
+//   }
+// }
+// })
+
+
